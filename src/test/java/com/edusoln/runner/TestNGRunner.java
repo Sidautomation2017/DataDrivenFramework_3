be041b4591361faf6c68f2534df7ef7d2ae35b93
Sidautomation2017 <sidheshwarpt@gmail.com>
2022-12-18 14:11:35 +0530

    runner class reupdate

--- src/test/java/com/edusoln/runner/TestNGRunner.java
+++ src/test/java/com/edusoln/runner/TestNGRunner.java
@@ -95,7 +95,6 @@ public class TestNGRunner extends CommonMethods {
 		report.flush();
 		log.info("reprot has been generated");
 		log.info("execution has been completed");
-		System.out.println("Execution is completed");
 	}
 	
 }
